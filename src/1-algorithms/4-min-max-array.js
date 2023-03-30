ae768dbba15506bb4579f5bc4a2023d2aa2ac12a
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-03-30 15:05:42 +0600

    time complexity going on

---
+++ src/1-algorithms/4-min-max-array.js
@@ -0,0 +1,26 @@
+const myArray = [4, 5, 1, 7, 2, 6, 3, 8];
+
+let minimum = Infinity; // Operation 1
+let maximum = -Infinity;  // Operation 1
+
+for (let num of myArray) {
+   if (num < minimum) // Operation 1
+      minimum = num; // Operation 1
+   
+   if (num > maximum) // Operation 1
+      maximum = num;  // Operation 1
+}
+console.log(`Min value ${minimum}`);
+console.log(`Max value ${maximum}`);
+
+function minValue() {
+   let minimum = Infinity; // Operation 1
+   for (let num of myArray) {
+      if (num < minimum) { // Operation 1
+         minimum = num; // Operation 1
+      }
+   }
+   return minimum; // Operation 1
+}
+// BigO >> T(n) = 1 + n (1 + 1) + 1 = 1 + 2n + 1 = 2n + 2 => O(n)
+console.log(minimum)
