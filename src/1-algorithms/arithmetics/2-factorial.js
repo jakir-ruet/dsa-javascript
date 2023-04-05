fc0bf262ba818ad84e559d585a4cd0f221c6b92b
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-04-05 11:09:40 +0600

    time complexity of factorial done

--- src/1-algorithms/arithmetics/2-factorial.js
+++ src/1-algorithms/arithmetics/2-factorial.js
@@ -1,10 +1,15 @@
 function factorial(n) {
-   let result = 1;
-   for (let i = 2; i <= n; i++) {
-      result = result * i;
+   let result = 1; //operation 1
+   for (let i = 2; i <= n; i++) { //operation 1
+      result = result * i; //operation 1
    }
-   return result;
+   return result; //operation 1
 }
-console.log(factorial(0))
-console.log(factorial(5))
-console.log(factorial(6))
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(5));
+// Time complexity
+// BigO >> T(n) = 1 + n (1 + 1) + 1 = 1 + 2n + 1 = 2n + 2 => O(n)
