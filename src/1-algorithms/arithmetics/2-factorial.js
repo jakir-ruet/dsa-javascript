ff18357b9cf175496629168b3f86224870f5a17a
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-04-06 10:21:17 +0600

    factorial recurson done

--- src/1-algorithms/arithmetics/2-factorial.js
+++ src/1-algorithms/arithmetics/2-factorial.js
@@ -1,3 +1,4 @@
+// // applying iterative
 function factorial(n) {
    let result = 1; //operation 1
    for (let i = 2; i <= n; i++) { //operation 1
@@ -13,3 +14,19 @@ console.log(factorial(4))
 console.log(factorial(5));
 // Time complexity
 // BigO >> T(n) = 1 + n (1 + 1) + 1 = 1 + 2n + 1 = 2n + 2 => O(n)
+
+// applying recursive
+function recursiveFactorial(n) {
+   if (n === 0) { //operation 1
+      return 1; //operation 1
+   }
+   return n * recursiveFactorial(n - 1); //operation 1
+}
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(5));
+// Time complexity
+// BigO >> T(n) = 1 + n (1) + n(1) = 2n + 1 => O(n)
