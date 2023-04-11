5a37a1eda591450112856e65d1d1ce0e041abb58
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-04-11 12:28:10 +0600

    list & link list going on

---
+++ src/3-lists/1-list.js
@@ -0,0 +1,10 @@
+// cleate a list of user as an object
+let jakir = { name: 'Jakir Ahmed' },
+   jasim = { name: 'Jasim Ahmed' },
+   jakia = { name: 'Jakia Ahmed' };
+
+console.log(jakir);
+
+let myArray = ['Jakir', 'Robin', 'Jasim', 'Kabir'];
+console.log(myArray);
+console.log(myArray[0]);
